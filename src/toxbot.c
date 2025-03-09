64139930d5a287b5afba269af45a31d0b20ee064
liqsliu <liqsliu@gmail.com>
2025-03-09 14:24:33 +0800

    20250309_142433: commit by u0_a128/localhost/Android/Linux/aarch64

--- src/toxbot.c
+++ src/toxbot.c
@@ -328,8 +328,10 @@ static void cb_group_invite2(
         log_timestamp("%d", sizeof(public_key));
         bool res = tox_group_self_get_public_key(m, PUBLIC_GROUP_NUM, (uint8_t *)public_key, NULL);
         log_timestamp("%d %X", sizeof(public_key), public_key);
-        log_timestamp("%d %lX", sizeof(public_key), public_key);
-        log_timestamp("%d %llX", sizeof(public_key), public_key);
+        for (int i=0; i<sizeof(public_key); i++)
+        {
+            printf("%hhX", public_key[i])
+        }
         sleep(3);
         log_timestamp("res: %x", res);
     } else {
