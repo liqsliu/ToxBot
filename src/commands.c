f717b7c136e4fe5552283740b5dabc6f47e28c60
liqsliu <liqsliu@gmail.com>
2025-03-10 19:17:15 +0800

    20250310_191715: commit by u0_a128/localhost/Android/Linux/aarch64

--- src/commands.c
+++ src/commands.c
@@ -894,9 +894,9 @@ static int my_parse_command(const char *input, char (*args)[MAX_COMMAND_LENGTH])
     free(cmd);
     /** j = sizeof(args)/sizeof(args[0]); */
     log_timestamp("length: %d", j);
-    printf("args: ");
+    printf("args:");
     for (i=0; i<num_args; ++i) {
-        printf("%s", args[i]);
+        printf(" \"%s\"", args[i]);
     }
     printf("\n");
     return num_args;
