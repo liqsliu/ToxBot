bf39013c13fc6bf2e229664c012797111a982c40
liqsliu <liqsliu@gmail.com>
2025-03-10 18:56:47 +0800

    20250310_185647: commit by liqsliu/wtfipfs.eu.org/GNU/Linux/Linux/x86_64

--- src/commands.c
+++ src/commands.c
@@ -939,24 +939,28 @@ int execute(Tox *m, uint32_t friendnum, const char *input, int length)
         return -1;
     }
 
+    char ** args;
     int num_args = -1;
     /** if (input[0] == '.' && input[1] != '\0') { */
     if (input[0] == '.') {
-        char args[MAX_NUM_ARGS][MAX_COMMAND_LENGTH];
+        char args2[MAX_NUM_ARGS][MAX_COMMAND_LENGTH];
+        args = args2
         num_args = my_parse_command(&input[1], args);
     } else if (strcmp(input, "invite") == 0) {
         /** char args[MAX_NUM_ARGS][MAX_COMMAND_LENGTH]; */
         /** int num_args = parse_command(input, args); */
-        char ** args={
+        char ** args2={
             "invite"
         };
+        args = args2
         num_args = 1;
     } else if (strcmp(input, "help") == 0) {
         /** char args[MAX_NUM_ARGS][MAX_COMMAND_LENGTH]; */
         /** int num_args = parse_command(input, args); */
-        char ** args={
+        char ** args2={
             "help"
         };
+        args = args2
         num_args = 1;
     } else {
         return -1;
