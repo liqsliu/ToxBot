9150c0c80b42e77be8446e2ce3df1420d0d4b028
liqsliu <liqsliu@gmail.com>
2025-03-11 22:02:35 +0800

    20250311_220235: commit by u0_a128/localhost/Android/Linux/aarch64

--- src/commands.c
+++ src/commands.c
@@ -481,14 +481,12 @@ static void cmd_init(Tox *m, uint32_t friendnumber, int argc, char (*argv)[MAX_C
     /*     log_timestamp("已忽略命令: %d %s", friendnumber, argv[0]); */
     /*     return; */
     /* } */
-    if (PUBLIC_GROUP_NUM == UINT32_MAX)
-    {
-        join_public_group(m);
-        log_timestamp("join: %s", CHAT_ID);
-    } else {
-        rejoin_public_group(m, PUBLIC_GROUP_NUM);
-        log_timestamp("rejoin: %d", PUBLIC_GROUP_NUM);
-    }
+    /** if (PUBLIC_GROUP_NUM == UINT32_MAX) */
+    join_public_group(m);
+    /** log_timestamp("join: %s", CHAT_ID); */
+    /** } else { */
+    /**     rejoin_public_group(m, PUBLIC_GROUP_NUM); */
+    /**     log_timestamp("rejoin: %d", PUBLIC_GROUP_NUM); */
     const char *outmsg =NULL;
     if (joined_group == true)
         outmsg = "ok";
